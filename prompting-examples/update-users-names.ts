0a8f0230db3ba28b668c2e44d7764f9d67ece65f
Yaroslav <yar.dobroskok@gmail.com>
2023-11-08 17:25:26 +0200

    Add mongoose connect

--- prompting-examples/update-users-names.ts
+++ prompting-examples/update-users-names.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const UserModel = mongoose.model('User');
 
 async function updateUsersNames() {
+  mongoose.connect('mongodb://localhost:27017/mongoose-examples');
   const users = await UserModel.find({}).exec();
 
   for (const user of users) {
